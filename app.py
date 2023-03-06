3abc3ff473225ea928fe53f05574e534c91ccdf8
FemboyBruher1337 <101051136+FemboyBruher1337@users.noreply.github.com>
2023-03-06 08:45:34 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+
+
+import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from PyQt5.QtCore import *
+
+from PyQt5.QtGui import *
+
+from PyQt5.QtWebEngine import *
+
+from PyQt5.QtWebEngineWidgets import *
+
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+app = QApplication(sys.argv)
+
+app.setApplicationName('RammerHead Browser')
+
+web = QWebEngineView()
+
+web.load(QUrl("https://browser.rammerhead.org"))
+
+web.show()
+
+sys.exit(app.exec_())
+
